7b92cda449b190705fbd2f134718a410f15da936
Ilya Kurinov <70485979+iku-work@users.noreply.github.com>
2021-05-24 16:08:52 +0300

    Update tutorial_baselines.py

--- m_gym/tutorial_baselines.py
+++ m_gym/tutorial_baselines.py
@@ -2,26 +2,28 @@ import gym
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
-
 from time import sleep
 
-# Run tutorial Jib Crane model 
+# Run WheelLoader environment
 sim = gym.make('m_gym:wheelloader-v0')
 env = DummyVecEnv([lambda: sim])
 
+# Wait till Mevea Solver opens
 sleep(10)
 
 model = PPO2(MlpPolicy, env, verbose=1)
 model.learn(total_timesteps=200000)
 
+# Reset an environment
 obs = env.reset()
 
 for i in range(20000):
-
+  
+  # Skip frame
   sleep(1)
+  
   action, _states = model.predict(obs)
   obs, rewards, done, info = env.step(action)
-
 
 env.close()
 
